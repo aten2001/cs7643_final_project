c5d0a606d4429e5b00fa06a6fb5e883a6a128543
tevon <twalker81@gatech.edu>
2019-11-21 09:55:59 -0500

    Move loss functions to separate file

--- generative_models/variational_autoencoder/utils.py
+++ generative_models/variational_autoencoder/utils.py
@@ -2,22 +2,30 @@
 Utilies for training
 """
 
+import torch
 import numpy as np
+from loss_functions import loss_function
+	
 
-def dfc_loss():
-	pass
-
-def KLD_loss(mu, logvar):
+def train_model(model, training_loader, optimizer, epochs, device, use_dfc_loss=False):
 	"""
-	Calculate KL-divergence loss
+	Function to train model
 	"""
-	kld = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(logvar)
-	kld = torch.sum(kld).mul_(-0.5)
-
-	return kld
-
-def train_model(model, training_dataloader, optimizer, epochs):
-	pass
+	model.train()
+    train_loss = 0
+    for batch_idx, (data, _) in enumerate(train_loader):
+        data = data.to(device)
+        optimizer.zero_grad()
+        recon_batch, mu, logvar = model(data)
+        loss = loss_function(recon_batch, data, mu, logvar)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                loss.item() / len(data)))
 
 def plot_loss(loss):
 	"""
