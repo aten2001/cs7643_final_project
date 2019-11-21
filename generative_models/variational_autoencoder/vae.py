0326900967dceb2b053bb71fa750cf20d6a5014c
tevon <twalker81@gatech.edu>
2019-11-21 09:00:09 -0500

    Add encoding definition

--- generative_models/variational_autoencoder/vae.py
+++ generative_models/variational_autoencoder/vae.py
@@ -6,24 +6,56 @@ class VariationalAutoencoder(nn.Module):
 	
 	Class to implement variational autoencoder
 	"""
-	def __init__(self, arg, latent_dim, use_dfc):
+	def __init__(self, arg, latent_dim, use_dfc, input_channels=3):
 		super(VariationalAutoencoder, self).__init__()
 		self.latent_dim = latent_dim
 		self.use_dfc = use_dfc
+		self.input_channels = input_channels
 
 		# Encoder
 
-		self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=2)
-		self.conv1 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2)
-		self.conv1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2)
+		# Convolutions
+		self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=32, kernel_size=3, stride=2)
+		self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2)
+		self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2)
 
-		self.fc1 = nn.Linear(1000, 512)
-		self.fc2 = nn.Linear(512, self.latent_dim)
+		# Fully connected
+		self.fc1 = nn.Linear(512, self.latent_dim)
+
+		#Activations
+		self.relu = nn.ReLU()
+		self.leaky_relu = nn.LeakyReLU(0.2)
 
 		# Decoder
+		
+		# Fully Connected
+		self.fc_mu = nn.Linear(128, self.latent_dim)
+		self.fc_sig = nn.Linear(128, self.latent_dim)
 
-		self.fc3 = nn.Linear(self.latent_dim, 128)
-		self.fc3 = nn.Linear(128, 512)
+	def encode(self, x, leaky=False):
+		"""
+		Encode image
+		"""
+
+		x = self.conv1(x)
+		if (leaky):
+			x = self.leaky_relu(x)
+		else:
+			x = self.relu(x)
+
+		x = self.conv2(x)
+		if (leaky):
+			x = self.leaky_relu(x)
+		else:
+			x = self.relu(x)
+
+		x = self.conv3(x)
+		if (leaky):
+			x = self.leaky_relu(x)
+		else:
+			x = self.relu(x)
+
+		return self.fc_mu(x), self.fc_sig(x)
 
 	def forward(self):
 		"""
