2d039f43b3f41c5f0371e9e9c6ced477dbfcd917
Lane <ldalan3@gatech.edu>
2019-12-01 16:43:04 -0500

    simple deconv

---
+++ generative_models/deconv.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+from data import DataSet
+
+class Deconv(nn.Module):
+    """
+    Decoder for the auto-encoder/decoder network
+    ref https://zo7.github.io/blog/2016/09/25/generating-faces.html
+    """
+    def __init__(self):  # , input_dim, hidden_dim, num_layers, linear_out, de_conv_stuff, batch_size):
+        super(Deconv, self).__init__()
+        self.linear_1 = nn.
+        self.unpool_1 = nn.Upsample(scale_factor=5, mode='bilinear')
+        self.deconv_1 = nn.ConvTranspose2d(in_channels=1, out_channels=3, kernel_size=200, stride=1)
+        # self.input_dim = input_dim
+        # self.hidden_dim = hidden_dim
+        # self.num_layers = num_layers
+        # self.linear_out = linear_out
+        # # self.de_conv_stuff, \  TODO DO ME!!
+        # self.batch_size = batch_size
+        #
+        # self.lstm = nn.LSTM(input_size=z_dim,
+        #                     hidden_size=hidden_dim,
+        #                     num_layers=num_layers)  # TODO: Consider dropout
+        # self.linear = nn.Linear(in_features=hidden_dim,
+        #                         out_features=linear_out)
+        # self.de_conv_1 = nn.ConvTranspose2d(in_channels=linear_out,
+        #                                     out_channels=3,
+        #                                     kernel_size=5)
+
+    def forward(self, input_vec):
+        output = input_vec.view(1, 1, 5, 5)
+        output = self.unpool_1(output)
+        output = self.deconv_1(output)
+
+        return output
+
+
+model = Deconv()
+criterion = F.mse_loss
+# criterion = F.binary_cross_entropy
+optimizer = optim.SGD(model.parameters(), lr=.02, momentum=0, weight_decay=0)
+
+tensor_1 = torch.ones(25)
+
+# Set values for DataSet object.
+seq_length = 1
+class_limit = 1  # Number of classes to extract. Can be 1-101 or None for all.
+video_limit = 1  # Number of videos allowed per class.  None for no limit
+data = DataSet(seq_length=seq_length, class_limit=class_limit, video_limit=video_limit)
+
+video_array = None
+for video in data.data:
+    video_array = data.video_to_vid_array(video)  # Get numpy array of sequence
+    break  # Only need one video to begin with.
+
+video_tensor = torch.from_numpy(video_array).type(torch.float32)
+
+model.train()
+for index in range(200):
+    optimizer.zero_grad()
+
+    output = model(tensor_1)
+    # print(video_tensor.dtype)
+    # print(output.dtype)
+    loss = criterion(output, video_tensor)
+    print("Step: {}, Loss: {}".format(index, loss))
+    loss.backward()
+    optimizer.step()
+
+model.eval()
+output = model(tensor_1)
+array_video = output.detach().numpy()
+data.vid_array_to_video("test_me", array_video)
+
+
--- generative_models/scratch.py
+++ generative_models/scratch.py
@@ -1,17 +1,42 @@
 import torchvision
 import torch
 import os
+import torchvision.models as models
 
-print(os.path.exists('data/ucfTrainTestlist/trainlist01.txt'))
+stride_1 = 1.0
+padding_1 = 0.0
+dilation_1 = 1.0
+kernel_size_1 = 200.0
+outpadding_1 = 0.0
+H_out = 224.0
 
-clip_length = 5
+# Deconv
+H_in = - (-stride_1 - 2 * padding_1 + dilation_1 * (kernel_size_1 - 1) + outpadding_1 + 1 - H_out) / stride_1
+print(H_in)
+print(H_in * H_in)
 
-ucf101_data = torchvision.datasets.UCF101(root="data/",  annotation_path="data/ucfTrainTestlist",
-                      frames_per_clip=clip_length, frame_rate=None, step_between_clips=clip_length, fold=2, train=True)
-train_loader = torch.utils.data.DataLoader(ucf101_data,
-                                           batch_size=1,
-                                           shuffle=True)
+scale_factor_2 = 5
+
+H_out = H_in
+H_in = H_out / scale_factor_2
+print(H_in)
+print(H_in * H_in)
 
 
-for batch_idx, (video, audio, label) in enumerate(train_loader):
-    print("hello world")
+# vgg16 = models.vgg16()
+#
+# print(os.path.exists('data/ucfTrainTestlist/trainlist01.txt'))
+#
+# clip_length = 5
+
+
+
+# ucf101_data = torchvision.datasets.UCF101(root="data/",  annotation_path="data/ucfTrainTestlist",
+#                       frames_per_clip=clip_length, frame_rate=None, step_between_clips=clip_length, fold=2, train=True)
+# train_loader = torch.utils.data.DataLoader(ucf101_data,
+#                                            batch_size=1,
+#                                            shuffle=True)
+#
+#
+# for batch_idx, (video, audio, label) in enumerate(train_loader):
+#     print("hello world")
